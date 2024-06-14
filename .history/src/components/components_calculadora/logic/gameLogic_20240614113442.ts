c1932577b6e14eae31046d0160d08105a73611d8
Mmarciomiguel <marciorodrigo2@hotmail.com>
2024-06-14 11:50:26 +0100

    alterações finais as logicas a trabalhar ate ao campo 2

--- .history/src/components/components_calculadora/logic/gameLogic_20240614113442.ts
+++ .history/src/components/components_calculadora/logic/gameLogic_20240614113442.ts
@@ -29,12 +29,8 @@ export const iniciarProximoJogo = (jogos: CampoType[][], setJogos: React.Dispatc
 
     // Vencedores e perdedores dos campos
     const vencedoresCampo1 = time1Campo1.resultado > time2Campo1.resultado ? time1Campo1.jogadores : time2Campo1.jogadores;
-    const perdedoresCampo1 = time1Campo1.resultado > time2Campo1.resultado ? time2Campo1.jogadores : time1Campo1.jogadores;
-
     const vencedoresCampo2 = time1Campo2.resultado > time2Campo2.resultado ? time1Campo2.jogadores : time2Campo2.jogadores;
     const perdedoresCampo2 = time1Campo2.resultado > time2Campo2.resultado ? time2Campo2.jogadores : time1Campo2.jogadores;
-
-    const vencedoresCampo3 = time1Campo3.resultado > time2Campo3.resultado ? time1Campo3.jogadores : time2Campo3.jogadores;
     const perdedoresCampo3 = time1Campo3.resultado > time2Campo3.resultado ? time2Campo3.jogadores : time1Campo3.jogadores;
 
     // Atualizar pontuação dos jogadores
@@ -52,7 +48,7 @@ export const iniciarProximoJogo = (jogos: CampoType[][], setJogos: React.Dispatc
       jogador.pontos += margemVitoriaCampo2;
     });
 
-    vencedoresCampo3.forEach(jogador => {
+    perdedoresCampo3.forEach(jogador => {
       jogador.vitorias += 1;
       jogador.pontos += margemVitoriaCampo3;
     });
@@ -63,16 +59,16 @@ export const iniciarProximoJogo = (jogos: CampoType[][], setJogos: React.Dispatc
       { jogadores: [vencedoresCampo1[1], vencedoresCampo2[1]], resultado: 0 }
     );
 
-    // Campo 2: perdedores do campo 1 e vencedores do campo 3
+    // Campo 2: vencedores do campo 2 e perdedores do campo 2
     novosCampos[1].times.push(
-      { jogadores: [perdedoresCampo1[0], vencedoresCampo3[0]], resultado: 0 },
-      { jogadores: [perdedoresCampo1[1], vencedoresCampo3[1]], resultado: 0 }
+      { jogadores: [vencedoresCampo2[0], perdedoresCampo2[0]], resultado: 0 },
+      { jogadores: [vencedoresCampo2[1], perdedoresCampo2[1]], resultado: 0 }
     );
 
-    // Campo 3: perdedores do campo 2 e perdedores do campo 3
+    // Campo 3: perdedores do campo 3 e perdedores do campo 2
     novosCampos[2].times.push(
-      { jogadores: [perdedoresCampo2[0], perdedoresCampo3[0]], resultado: 0 },
-      { jogadores: [perdedoresCampo2[1], perdedoresCampo3[1]], resultado: 0 }
+      { jogadores: [perdedoresCampo3[0], perdedoresCampo2[0]], resultado: 0 },
+      { jogadores: [perdedoresCampo3[1], perdedoresCampo2[1]], resultado: 0 }
     );
   }
 
