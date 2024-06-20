d6cf40ed68f6d0da14c18eebbe7ad940fb9c2f95
Mmarciomiguel <marciorodrigo2@hotmail.com>
2024-06-20 11:29:06 +0100

    feat: Atualizar estilos CSS do botão de trocar jogadores e adicionar animações

--- .history/src/pages/Home/Home_20240620111323.tsx
+++ .history/src/pages/Home/Home_20240620111323.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
 import padelImage from '../../assets/images/inicio.jpg';
-import Titulo from '../../components/components_calculadora/Titulo';
+
 const PadelPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <div className={styles.padelContainer}>
       <div className={styles.centeredContent}>
-      <Titulo texto="PadelMaster" className={styles.tennisBall} />
-
+        <h1>PadelMaster</h1>
         <div className={styles.content}>
           <img src={padelImage} alt="Imagem de Padel" className={styles.padelImage} />
           <div className={styles.buttonContainer}>
