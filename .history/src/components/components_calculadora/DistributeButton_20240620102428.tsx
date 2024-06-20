fc3baa1b216d3b4af3b1feccd16a27d3d252fa31
Mmarciomiguel <marciorodrigo2@hotmail.com>
2024-06-20 10:44:27 +0100

    feat: Atualizar estilos CSS do botão de trocar jogadores e adicionar animações

--- .history/src/components/components_calculadora/DistributeButton_20240620102428.tsx
+++ .history/src/components/components_calculadora/DistributeButton_20240620102428.tsx
@@ -3,19 +3,13 @@ import tennisBall from '../../assets/images/tennis-ball.png';
 import styles from './DistributeButton.module.css'; // Importando o arquivo CSS Module
 
 const DistributeButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
-  // Estado para rastrear se a bola está girando
   const [isRotating, setIsRotating] = useState(false);
 
-  // Função chamada ao clicar no botão
   const handleClick = () => {
-    // Definindo estado de rotação como verdadeiro para iniciar a animação
     setIsRotating(true);
-
-    // Após o tempo da animação (1 segundo), definir rotação como falso e chamar onClick
+    onClick();
     setTimeout(() => {
       setIsRotating(false);
-      // Chama a função onClick somente após a animação completar
-      onClick();
     }, 1000); // Tempo da animação em milissegundos
   };
 
@@ -25,7 +19,6 @@ const DistributeButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
         <img
           src={tennisBall}
           alt="Tennis Ball"
-          // Adiciona a classe 'rotate' condicionalmente baseado no estado isRotating
           className={`${styles.tennisBall} ${isRotating ? styles.rotate : ''}`}
         />
         <span className={styles.label}>Distribuir Jogadores</span>
