3f2b0b073ce7d5053f24324c489373b951a4f187
Mmarciomiguel <marciorodrigo2@hotmail.com>
2024-06-27 22:51:19 +0100

    botoes seletores

---
+++ .history/src/components/components_calculadora/PlayerSelector_20240627224450.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import styles from './PlayerSelector.module.css';
+import tennisBall from '/home/marciomiguel/code/MarcioMiguel22/Sites/PadelMaster/src/assets/images/tennis-ball.png';
+
+const PlayerSelector: React.FC = () => {
+  const [selectedOption, setSelectedOption] = useState<number>(4);
+
+  const handleClick = () => {
+    setSelectedOption(prevOption => {
+      if (prevOption === 4) return 8;
+      if (prevOption === 8) return 12;
+      return 4;
+    });
+  };
+
+  return (
+    <div className={styles.selectorContainer} onClick={handleClick}>
+      <div className={styles.bolaContainer}>
+        <img
+          src={tennisBall}
+          alt="Bola de Tênis"
+          className={`${styles.bola} ${
+            selectedOption === 4 ? styles.option4 : selectedOption === 8 ? styles.option8 : styles.option12
+          }`}
+        />
+      </div>
+      <div className={styles.options}>
+        <div className={styles.optionText}>4 Jogadores</div>
+        <div className={styles.optionText}>8 Jogadores</div>
+        <div className={styles.optionText}>12 Jogadores</div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerSelector;
